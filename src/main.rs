e7350c0114292ebdebb8cf5ac49e17745f5c98e6
Timshel <Timshel@users.noreply.github.com>
2024-02-16 19:27:27 +0100

    Specific sso nonce purge + Zitadel doc

--- src/main.rs
+++ src/main.rs
@@ -572,7 +572,6 @@ fn schedule_jobs(pool: db::DbPool) {
             if !CONFIG.trash_purge_schedule().is_empty() {
                 sched.add(Job::new(CONFIG.trash_purge_schedule().parse().unwrap(), || {
                     runtime.spawn(api::purge_trashed_ciphers(pool.clone()));
-                    runtime.spawn(db::models::SsoNonce::delete_expired(pool.clone()));
                 }));
             }
 
@@ -614,6 +613,13 @@ fn schedule_jobs(pool: db::DbPool) {
             {
                 sched.add(Job::new(CONFIG.event_cleanup_schedule().parse().unwrap(), || {
                     runtime.spawn(api::event_cleanup_job(pool.clone()));
+                }));
+            }
+
+            // Purge sso nonce from incomplete flow (default to daily at 00h20).
+            if !CONFIG.purge_incomplete_sso_nonce().is_empty() {
+                sched.add(Job::new(CONFIG.purge_incomplete_sso_nonce().parse().unwrap(), || {
+                    runtime.spawn(db::models::SsoNonce::delete_expired(pool.clone()));
                 }));
             }
 
