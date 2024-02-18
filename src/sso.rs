4c8ed668d42d22205611e845e871d550a3e2af88
Timshel <Timshel@users.noreply.github.com>
2024-02-18 15:56:04 +0100

    Remove openidconnect client cache

--- src/sso.rs
+++ src/sso.rs
@@ -1,7 +1,6 @@
 use chrono::Utc;
 use regex::Regex;
 use std::borrow::Cow;
-use std::sync::RwLock;
 use std::time::Duration;
 use url::Url;
 
@@ -29,8 +28,6 @@ use crate::{
 
 static AC_CACHE: Lazy<Cache<String, AuthenticatedUser>> =
     Lazy::new(|| Cache::builder().max_capacity(1000).time_to_live(Duration::from_secs(10 * 60)).build());
-
-static CLIENT_CACHE: RwLock<Option<CoreClient>> = RwLock::new(None);
 
 static SSO_JWT_ISSUER: Lazy<String> = Lazy::new(|| format!("{}|sso", CONFIG.domain_origin()));
 
@@ -138,8 +135,7 @@ fn decode_token_claims(token_name: &str, token: &str) -> ApiResult<BasicTokenCla
 
 #[rocket::async_trait]
 trait CoreClientExt {
-    async fn _get_client() -> ApiResult<CoreClient>;
-    async fn cached() -> ApiResult<CoreClient>;
+    async fn get() -> ApiResult<CoreClient>;
 
     async fn user_info_async(&self, access_token: AccessToken) -> ApiResult<CoreUserInfoClaims>;
 
@@ -149,7 +145,7 @@ trait CoreClientExt {
 #[rocket::async_trait]
 impl CoreClientExt for CoreClient {
     // Call the OpenId discovery endpoint to retrieve configuration
-    async fn _get_client() -> ApiResult<CoreClient> {
+    async fn get() -> ApiResult<CoreClient> {
         let client_id = ClientId::new(CONFIG.sso_client_id());
         let client_secret = ClientSecret::new(CONFIG.sso_client_secret());
 
@@ -162,19 +158,6 @@ impl CoreClientExt for CoreClient {
 
         Ok(CoreClient::from_provider_metadata(provider_metadata, client_id, Some(client_secret))
             .set_redirect_uri(CONFIG.sso_redirect_url()?))
-    }
-
-    // Simple cache to prevent recalling the discovery endpoint each time
-    async fn cached() -> ApiResult<CoreClient> {
-        let cc_client = CLIENT_CACHE.read().ok().and_then(|rw_lock| rw_lock.clone());
-        match cc_client {
-            Some(client) => Ok(client),
-            None => Self::_get_client().await.map(|client| {
-                let mut cached_client = CLIENT_CACHE.write().unwrap();
-                *cached_client = Some(client.clone());
-                client
-            }),
-        }
     }
 
     async fn user_info_async(&self, access_token: AccessToken) -> ApiResult<CoreUserInfoClaims> {
@@ -223,7 +206,7 @@ pub async fn authorize_url(state: String, client_id: &str, raw_redirect_uri: &st
         _ => err!(format!("Unsupported client {client_id}")),
     };
 
-    let (auth_url, csrf_state, nonce) = CoreClient::cached()
+    let (auth_url, csrf_state, nonce) = CoreClient::get()
         .await?
         .authorize_url(
             AuthenticationFlow::<CoreResponseType>::AuthorizationCode,
@@ -299,7 +282,7 @@ pub async fn exchange_code(wrapped_code: &str, conn: &mut DbConn) -> ApiResult<U
     }
 
     let oidc_code = AuthorizationCode::new(code.clone());
-    let client = CoreClient::cached().await?;
+    let client = CoreClient::get().await?;
 
     let nonce = match SsoNonce::find(&state, conn).await {
         None => err!(format!("Invalid state cannot retrieve nonce")),
@@ -455,7 +438,7 @@ pub async fn exchange_refresh_token(
         Some(TokenWrapper::Refresh(refresh_token)) => {
             let rt = RefreshToken::new(refresh_token.to_string());
 
-            let client = CoreClient::cached().await?;
+            let client = CoreClient::get().await?;
 
             let token_response = match client.exchange_refresh_token(&rt).request_async(async_http_client).await {
                 Err(err) => err!(format!("Request to exchange_refresh_token endpoint failed: {:?}", err)),
@@ -484,7 +467,7 @@ pub async fn exchange_refresh_token(
                 err_silent!("Access token is close to expiration but we have no refresh token")
             }
 
-            let client = CoreClient::cached().await?;
+            let client = CoreClient::get().await?;
             match client.user_info_async(AccessToken::new(access_token.to_string())).await {
                 Err(err) => {
                     err_silent!(format!("Failed to retrieve user info, token has probably been invalidated: {err}"))
